7d5b3ab2e5c52a0c0dcdc3d9a47122894abf8b9a
Airidas Balkus <20107228@mail.wit.ie>
2025-10-07 12:36:29 +0100

    Show movie review excerpts; Refactor Details page.

--- movies/src/api/tmdb-api.jsx
+++ movies/src/api/tmdb-api.jsx
@@ -21,11 +21,20 @@ export const getGenres = () => {
     .then(res => res.json())
     .then(json => json.genres);
 };
-
 export const getMovieImages = (id) => {
   return fetch(
     `https://api.themoviedb.org/3/movie/${id}/images?api_key=${import.meta.env.VITE_TMDB_KEY}`
   )
     .then((res) => res.json())
     .then((json) => json.posters);
+};
+export const getMovieReviews = (id) => {
+  return fetch(
+    `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${import.meta.env.VITE_TMDB_KEY}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      // console.log(json.results);
+      return json.results;
+    });
 };
