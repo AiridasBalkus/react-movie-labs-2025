78307c750850c864734c88138ca8ebc100bf7dee
Airidas Balkus <20107228@mail.wit.ie>
2025-10-07 12:02:35 +0100

    Add to favorited logic complete

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -23,9 +23,12 @@ const HomePage = (props) => {
     if (type === "name") setNameFilter(value);
     else setGenreFilter(value);
   };
-
-
-
+  const addToFavorites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favorite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
   useEffect(() => {
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
@@ -54,7 +57,7 @@ const HomePage = (props) => {
     />
 
         </Grid>
-        <MovieList movies={displayedMovies}></MovieList>
+            <MovieList movies={displayedMovies} selectFavorite={addToFavorites} />
       </Grid>
     </Grid>
   );
