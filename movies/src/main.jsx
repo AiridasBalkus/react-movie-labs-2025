2b5392203d98395db1ff9128ed84f88283da6dc7
Airidas Balkus <20107228@mail.wit.ie>
2025-10-14 11:41:40 +0100

    Using react-query to manage the browser-based cache.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -9,6 +9,19 @@ import { useLocation } from "react-router";
 import PageTemplate from "../src/components/templateMoviePage";
 import MovieReview from "../src/components/movieReview";
 import SiteHeader from './components/siteHeader'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
 
 const MovieReviewPage = (props) => {
   let location = useLocation();
@@ -25,19 +38,22 @@ export default MovieReviewPage;
 
 const App = () => {
   return (
-    <BrowserRouter>
-    
-      <SiteHeader />
-      <Routes>
-        <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
-        <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-        <Route path="/movies/:id" element={<MoviePage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={ <Navigate to="/" /> } />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader />
+        <Routes>
+          <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+          <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
+          <Route path="/movies/:id" element={<MoviePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={ <Navigate to="/" /> } />
+        </Routes>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
+
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
